437758725c56f4574ce48a742145cba79dc6ae41
fidelicura <fidelicura@gmail.com>
2024-02-29 23:13:30 +0300

    -unused hier folder structure creator

--- bootstrap/hierarchy.sh
+++ bootstrap/hierarchy.sh
@@ -9,7 +9,4 @@ mkdir -p $HOME/.config
 mkdir -p $HOME/.local/share/fonts
 mkdir -p $HOME/.local/share/icons
 mkdir -p $HOME/.local/share/themes
-mkdir -p $HOME/.mozilla/firefox/main.main/chrome
-mkdir -p $HOME/.mozilla/firefox/main.main/assets
-mkdir -p $HOME/.mozilla/firefox/main.main/extensions
 echo -e "\n[$] > Hierarchy created successfully!"
