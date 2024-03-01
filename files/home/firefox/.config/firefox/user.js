e7d9d33f0af635cbde8dfe6d630c115e79e1f5a3
fidelicura <fidelicura@gmail.com>
2024-03-01 20:59:38 +0300

    +extensions folder, -userchrome, -usercontent

--- files/home/firefox/.config/firefox/user.js
+++ files/home/firefox/.config/firefox/user.js
@@ -261,9 +261,3 @@ user_pref("media.videocontrols.picture-in-picture.enabled", false);
 
 /** DISABLE INTERNAL LANGUAGE TRANSLATOR **/
 user_pref("browser.translations.neverTranslateLanguages", true);
-
-/** ENABLE GRUVBOX THEME **/
-user_pref("extensions.activeThemeID", "{d3f8e52b-140d-41e8-ae4a-a9710ab6a886}");
-
-/** USE OWN FONT **/
-user_pref("font.name.serif.x-western", "Roboto");
