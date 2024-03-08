1880a5b2f0bd439c3bdda751673246fabff68ac1
fidelicura <fidelicura@gmail.com>
2024-03-08 23:29:05 +0300

    +wezterm config (final?)

--- files/home/wezterm/.config/wezterm/wezterm.lua
+++ files/home/wezterm/.config/wezterm/wezterm.lua
@@ -5,17 +5,53 @@ local config = wezterm.config_builder()
 
 
 
+-- {{ GLOBALS }} --
+config.window_close_confirmation = "NeverPrompt"
+
+wezterm.on("gui-startup", function(_)
+  local mux = wezterm.mux
+
+  local first_tab, _, window = mux.spawn_window({ args = { "bash" } })
+  first_tab:set_title("main")
+
+  local second_tab = window:spawn_tab({ args = { "bash" } })
+  second_tab:set_title("auxiliary")
+
+  local third_tab = window:spawn_tab({ args = { "btm" } })
+  third_tab:set_title("monitoring")
+
+  first_tab:activate()
+end)
+-- {{ GLOBALS }} --
+
 -- {{ THEME }} --
 config.color_scheme = "GruvboxDark"
+
 config.font = wezterm.font("Iosevka NF")
 config.font_size = 14.0
+
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 30,
+  right = 30,
+  top = 50,
+  bottom = 50,
+}
 -- {{ THEME }} --
 
--- {{ ANIMATION }} --
-config.animation_fps = 120
+-- {{ ANIMATIONS }} --
+config.animation_fps = 1
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
--- {{ ANIMATION }} --
+-- {{ ANIMATIONS }} --
+
+-- {{ BINDINGS }} --
+local act = wezterm.action
+config.keys = {
+  { key = "j", mods = "ALT", action = act.ActivateTabRelative(-1) },
+  { key = "k", mods = "ALT", action = act.ActivateTabRelative(1) },
+}
+-- {{ BINDINGS }} --
 
 
 
