41946123118d047cce7129718cad597c6641f918
fidelicura <fidelicura@gmail.com>
2024-05-03 09:37:38 +0300

    improve

--- files/home/wezterm/.config/wezterm/wezterm.lua
+++ files/home/wezterm/.config/wezterm/wezterm.lua
@@ -28,8 +28,9 @@ end)
 config.color_scheme = "GruvboxDark"
 config.enable_tab_bar = false
 
-config.font = wezterm.font "Iosevka NF"
+config.font = wezterm.font "IBM Plex Mono"
 config.font_size = 12.0
+config.cell_width = 0.9
 
 config.window_background_opacity = 0.85
 config.window_padding = {
@@ -46,6 +47,7 @@ config.max_fps = 120
 config.prefer_egl = true
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
+config.hide_mouse_cursor_when_typing = false
 -- {{ ANIMATIONS }} --
 
 -- {{ BINDINGS }} --
