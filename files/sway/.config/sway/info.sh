b2248fb08657b9283a4a9a830f726f44edf82e75
fidelicura <fidelicura@gmail.com>
2024-05-11 11:21:05 +0300

    improve info script for sway

--- files/sway/.config/sway/info.sh
+++ files/sway/.config/sway/info.sh
@@ -3,8 +3,8 @@
 while :; do
 	DATETIME="$(date +'%Y-%m-%d %H:%M:%S')"
 	BATTERY="$(upower -i `upower -e | grep 'BAT'` | grep -E "percentage" | grep -oP '\d+(%)')"
-	INPUT_VOLUME="$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)"
-	OUTPUT_VOLUME="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
+	INPUT_VOLUME="$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -oP '\d+(...)')"
+	OUTPUT_VOLUME="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oP '\d+(...)')"
 
 	echo "OUT: $OUTPUT_VOLUME | IN: $INPUT_VOLUME | BAT: $BATTERY | $DATETIME"
 	sleep 0.05
